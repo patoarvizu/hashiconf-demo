b101533be786d7f08a65a39eb97e35e8630e2189
patoarvizu <patoarvizu@gmail.com>
2020-09-17 00:47:46 -0400

    First set of files

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	vaultapi "github.com/hashicorp/vault/api"
+)
+
+func main() {
+	// time.Sleep(5 * time.Second)
+	client, err := vaultapi.NewClient(vaultapi.DefaultConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		secret, err := client.Logical().Read(os.Getenv("SECRET_PATH"))
+		if err != nil {
+			fmt.Fprint(w, "I can't read that secret :(")
+		} else {
+			fmt.Fprint(w, secret.Data["hello"])
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
